f63476ee9dde0b1c312b22e97b198a915cda4811
Łukasz Sobieski <Lukasz.Sobieski@webcon.com>
2025-11-05 19:42:55 +0100

    Add RandomUserMappings

--- RandomUserService.Infrastructure/External/RandomUserApiClient.cs
+++ RandomUserService.Infrastructure/External/RandomUserApiClient.cs
@@ -2,6 +2,7 @@
 using RandomUserService.Core.Interfaces;
 using System.Net.Http.Json;
 using RandomUserService.Infrastructure.External.Models;
+using RandomUserService.Infrastructure.Mappings;
 
 namespace RandomUserService.Infrastructure.External
 {
@@ -18,22 +19,13 @@ namespace RandomUserService.Infrastructure.External
         public async Task<User> GetRandomUserAsync()
         {
             var response = await _httpClient.GetFromJsonAsync<RandomUserResponse>(Url);
-            var apiUser = response?.Results?.FirstOrDefault();
-            if (apiUser == null)
+            var randomUser = response?.Results.FirstOrDefault();
+            if (randomUser == null)
             {
                 throw new Exception("No user returned");
             }
 
-            return new User
-            {
-                Title = apiUser.Name.Title,
-                FirstName = apiUser.Name.First,
-                LastName = apiUser.Name.Last,
-                Gender = apiUser.Gender,
-                Email = apiUser.Email,
-                ExternalId = apiUser.Login.Uuid,
-                Timestamp = DateTime.UtcNow
-            };
+            return randomUser.ToDomain();
         }
     }
 }
---
+++ RandomUserService.Infrastructure/Mappings/RandomUserMappings.cs
@@ -0,0 +1,22 @@
+﻿using RandomUserService.Core.Entities;
+using RandomUserService.Infrastructure.External.Models;
+
+namespace RandomUserService.Infrastructure.Mappings
+{
+    internal static class RandomUserMappings
+    {
+        public static User ToDomain(this RandomUser result)
+        {
+            return new User
+            {
+                Title = result.Name.Title,
+                FirstName = result.Name.First,
+                LastName = result.Name.Last,
+                Gender = result.Gender,
+                Email = result.Email,
+                ExternalId = result.Login.Uuid,
+                Timestamp = DateTime.UtcNow
+            };
+        }
+    }
+}
